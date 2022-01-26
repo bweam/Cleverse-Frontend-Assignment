3e3f371a088ff98de9795e1ac45ff2ed4b79f75a
supat <supatachbeam@gmail.com>
2022-01-27 00:41:16 +0700

    fixed flight-logs not appearing on card

---
+++ frontend-screening-assignment/.next/static/webpack/webpack.e2e272767a8d724080d6.hot-update.js
@@ -0,0 +1,11 @@
+"use strict";
+self["webpackHotUpdate_N_E"]("webpack",{},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ /* webpack/runtime/getFullHash */
+/******/ !function() {
+/******/ 	__webpack_require__.h = function() { return "4dc3b324ea47f150e2df"; }
+/******/ }();
+/******/ 
+/******/ }
+);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoic3RhdGljL3dlYnBhY2svd2VicGFjay5lMmUyNzI3NjdhOGQ3MjQwODBkNi5ob3QtdXBkYXRlLmpzIiwibWFwcGluZ3MiOiI7Ozs7O1VBQUEscUNBQXFDIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vX05fRS93ZWJwYWNrL3J1bnRpbWUvZ2V0RnVsbEhhc2giXSwic291cmNlc0NvbnRlbnQiOlsiX193ZWJwYWNrX3JlcXVpcmVfXy5oID0gZnVuY3Rpb24oKSB7IHJldHVybiBcIjRkYzNiMzI0ZWE0N2YxNTBlMmRmXCI7IH0iXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
--- frontend-screening-assignment/src/flight-log.js
+++ frontend-screening-assignment/src/flight-log.js
@@ -1,29 +1,29 @@
 export class FlightLogService {
-  initialData = [
-    {
-      passengerName: "cherprang",
-      airport: "bangkok",
-      timestamp: 1630454400,
-      type: "departure",
-    },
-    {
-      passengerName: "sita",
-      airport: "chiangmai",
-      timestamp: 1630627200,
-      type: "departure",
-    },
-    {
-      passengerName: "cherprang",
-      airport: "tokyo",
-      timestamp: 1630454405,
-      type: "arrival",
-    },
-  ];
 
   getLogs() {
+    let initialData = [
+      {
+        passengerName: "cherprang",
+        airport: "bangkok",
+        timestamp: 1630454400,
+        type: "departure",
+      },
+      {
+        passengerName: "sita",
+        airport: "chiangmai",
+        timestamp: 1630627200,
+        type: "departure",
+      },
+      {
+        passengerName: "cherprang",
+        airport: "tokyo",
+        timestamp: 1630454405,
+        type: "arrival",
+      },
+    ];
     return new Promise(function (resolve) {
       setTimeout(function () {
-        resolve(this.initialData || []);
+        resolve(initialData || []);
       }, 2000);
     });
   }
